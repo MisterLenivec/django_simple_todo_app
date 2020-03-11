f6a387bdccdc70ecbc81814340cc4ca6c18fdd1e
lenivec <wormsom@gmail.com>
2020-03-11 22:32:12 +0300

    still try fix TimeoutException error in travis

--- tasks/tests/test_selenium.py
+++ tasks/tests/test_selenium.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 
 
 class SeleniumTests(TestCase):
@@ -28,19 +29,19 @@ class SeleniumTests(TestCase):
                 """
                 Create task
                 """
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.ID, 'id_title'))
                 ).send_keys(title_val)
 
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.ID, 'id_description'))
                 ).send_keys(desc_val)
 
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.element_to_be_clickable((By.NAME, 'create_task'))
                 ).click()
 
-                message_create = WebDriverWait(self.browser, 10).until(
+                message_create = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
                 )
                 assert 'Задача успешно создана!' in message_create.text
@@ -48,7 +49,7 @@ class SeleniumTests(TestCase):
                 """
                 Number of cards increased by 1
                 """
-                number_cards = WebDriverWait(self.browser, 10).until(
+                number_cards = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.TAG_NAME, 'p'))
                 )
                 assert "{} карточек".format(
@@ -66,14 +67,14 @@ class SeleniumTests(TestCase):
                 """
                 Update task
                 """
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'edit'))
                 ).click()
 
                 """
                 Message on edit page
                 """
-                message_edit = WebDriverWait(self.browser, 10).until(
+                message_edit = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
                 )
                 assert 'Редактировать' in message_edit.text
@@ -81,12 +82,14 @@ class SeleniumTests(TestCase):
                 """
                 Specified text on the card
                 """
-                original_id_title = WebDriverWait(self.browser, 10).until(
+                original_id_title = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.ID, 'id_title'))
                 )
                 assert original_id_title.get_attribute('value') in title_val
 
-                original_id_description = WebDriverWait(self.browser, 10).until(
+                original_id_description = WebDriverWait(
+                    self.browser, 50
+                ).until(
                     EC.visibility_of_element_located((By.ID, 'id_description'))
                 )
                 assert original_id_description.text in desc_val
@@ -99,18 +102,18 @@ class SeleniumTests(TestCase):
                     'id_description'
                 ).send_keys(' edit')
 
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.ID, 'id_complete'))
                 ).click()
 
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.NAME, 'update_task'))
                 ).click()
 
                 """
                 Pop-up message on the main page
                 """
-                message_update = WebDriverWait(self.browser, 10).until(
+                message_update = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
                 )
                 assert 'Задача успешно обновлена!' in message_update.text
@@ -119,14 +122,14 @@ class SeleniumTests(TestCase):
                 Title and tag updated
                 """
                 card_update_text = self.browser.find_element_by_tag_name(
-                    'div.item-row > div > strike'
+                    'div > div > strike'
                 )
                 assert title_val + ' edit' in card_update_text.text
 
                 """
                 Description updated
                 """
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'edit'))
                 ).click()
                 assert desc_val + ' edit' in self.browser.find_element_by_id(
@@ -136,7 +139,7 @@ class SeleniumTests(TestCase):
                 """
                 The cancel button sent us to the main page
                 """
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((
                         By.CLASS_NAME, 'cancel_button'
                     ))
@@ -148,11 +151,11 @@ class SeleniumTests(TestCase):
                 """
                 Deleting task
                 """
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'delete'))
                 ).click()
 
-                message_delete = WebDriverWait(self.browser, 10).until(
+                message_delete = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
                 )
                 assert 'Удалить задачу' in message_delete.text
@@ -160,26 +163,26 @@ class SeleniumTests(TestCase):
                 """
                 Check if the visible description and buttons, deleting the task
                 """
-                delete_text = WebDriverWait(self.browser, 10).until(
+                delete_text = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.TAG_NAME, 'p'))
                 )
                 assert delete_text
 
-                cancel_delete = WebDriverWait(self.browser, 10).until(
+                cancel_delete = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((
                         By.CLASS_NAME, 'cancel_button'
                     ))
                 )
                 assert cancel_delete
 
-                WebDriverWait(self.browser, 10).until(
+                WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.NAME, 'confirm'))
                 ).click()
 
                 """
                 Message on the main page after delete
                 """
-                message_success_delete = WebDriverWait(self.browser, 10).until(
+                message_success_delete = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
                 )
                 assert 'Задача удалена!' in message_success_delete.text
@@ -187,12 +190,15 @@ class SeleniumTests(TestCase):
                 """
                 Checking the number of cards
                 """
-                number_cards = WebDriverWait(self.browser, 10).until(
+                number_cards = WebDriverWait(self.browser, 50).until(
                     EC.visibility_of_element_located((By.TAG_NAME, 'p'))
                 )
                 assert "{} карточек".format(
                     int(cards_val)
                 ) in number_cards.text
+
+            except TimeoutException as e:
+                print('[!] Timeout: ' + str(e))
 
             finally:
                 self.browser.quit()
