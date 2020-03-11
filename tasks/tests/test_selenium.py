c05e6333dd2b108c4cd2080a5d6ba5279345c3dd
lenivec <wormsom@gmail.com>
2020-03-11 21:18:50 +0300

    small changes in selenium tests

--- tasks/tests/test_selenium.py
+++ tasks/tests/test_selenium.py
@@ -1,188 +1,196 @@
+from django.test import TestCase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 
-def selenium_test(web):
-    title_val = 'Selenium test value'
-    desc_val = 'Selenium desc value'
-    cards_val = ''
+class SeleniumTests(TestCase):
 
-    try:
-        browser = web
-        browser.implicitly_wait(5)
-        browser.get('http://127.0.0.1:8000/')
+    def test_task_app_with_selenium(self):
+        for driver in [webdriver.Chrome(), webdriver.Firefox()]:
+            self.browser = driver
+            self.browser.implicitly_wait(5)
+            self.browser.get('http://127.0.0.1:8000/')
 
-        """
-        Number of cards from string
-        """
-        for i in browser.find_element_by_tag_name('p').text:
-            if i.isnumeric():
-                cards_val += str(i)
+            title_val = 'Selenium test value'
+            desc_val = 'Selenium desc value'
+            cards_val = ''
 
-        """
-        Create task
-        """
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.ID, 'id_title'))
-        ).send_keys(title_val)
+            try:
+                """
+                Number of cards from string
+                """
+                for i in self.browser.find_element_by_tag_name('p').text:
+                    if i.isnumeric():
+                        cards_val += str(i)
 
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.ID, 'id_description'))
-        ).send_keys(desc_val)
+                """
+                Create task
+                """
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.ID, 'id_title'))
+                ).send_keys(title_val)
 
-        WebDriverWait(browser, 3).until(
-            EC.element_to_be_clickable((By.NAME, 'create_task'))
-        ).click()
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.ID, 'id_description'))
+                ).send_keys(desc_val)
 
-        message_create = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
-        )
-        assert 'Задача успешно создана!' in message_create.text
+                WebDriverWait(self.browser, 3).until(
+                    EC.element_to_be_clickable((By.NAME, 'create_task'))
+                ).click()
 
-        """
-        Number of cards increased by 1
-        """
-        number_cards = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.TAG_NAME, 'p'))
-        )
-        assert "{} карточек".format(int(cards_val) + 1) in number_cards.text
+                message_create = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
+                )
+                assert 'Задача успешно создана!' in message_create.text
 
-        """
-        The card was created?
-        """
-        card_text = browser.find_element_by_tag_name(
-            'div.item-row > div > span'
-        )
-        assert title_val in card_text.text
+                """
+                Number of cards increased by 1
+                """
+                number_cards = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.TAG_NAME, 'p'))
+                )
+                assert "{} карточек".format(
+                    int(cards_val) + 1
+                ) in number_cards.text
 
-        """
-        Update task
-        """
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'edit'))
-        ).click()
+                """
+                The card was created?
+                """
+                card_text = self.browser.find_element_by_tag_name(
+                    'div.item-row > div > span'
+                )
+                assert title_val in card_text.text
 
-        """
-        Message on edit page
-        """
-        message_edit = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
-        )
-        assert 'Редактировать' in message_edit.text
+                """
+                Update task
+                """
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'edit'))
+                ).click()
 
-        """
-        Specified text on the card
-        """
-        original_id_title = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.ID, 'id_title'))
-        )
-        assert original_id_title.get_attribute('value') in title_val
+                """
+                Message on edit page
+                """
+                message_edit = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
+                )
+                assert 'Редактировать' in message_edit.text
 
-        original_id_description = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.ID, 'id_description'))
-        )
-        assert original_id_description.text in desc_val
+                """
+                Specified text on the card
+                """
+                original_id_title = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.ID, 'id_title'))
+                )
+                assert original_id_title.get_attribute('value') in title_val
 
-        """
-        Passing text to the fields and update the task
-        """
-        browser.find_element_by_id('id_title').send_keys(' edit')
-        browser.find_element_by_id('id_description').send_keys(' edit')
+                original_id_description = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.ID, 'id_description'))
+                )
+                assert original_id_description.text in desc_val
 
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.ID, 'id_complete'))
-        ).click()
+                """
+                Passing text to the fields and update the task
+                """
+                self.browser.find_element_by_id('id_title').send_keys(' edit')
+                self.browser.find_element_by_id(
+                    'id_description'
+                ).send_keys(' edit')
 
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.NAME, 'update_task'))
-        ).click()
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.ID, 'id_complete'))
+                ).click()
 
-        """
-        Pop-up message on the main page
-        """
-        message_update = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
-        )
-        assert 'Задача успешно обновлена!' in message_update.text
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.NAME, 'update_task'))
+                ).click()
 
-        """
-        Title and tag updated
-        """
-        card_update_text = browser.find_element_by_tag_name(
-            'div.item-row > div > strike'
-        )
-        assert title_val + ' edit' in card_update_text.text
+                """
+                Pop-up message on the main page
+                """
+                message_update = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
+                )
+                assert 'Задача успешно обновлена!' in message_update.text
 
-        """
-        Description updated
-        """
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'edit'))
-        ).click()
-        assert desc_val + ' edit' in browser.find_element_by_id(
-            'id_description'
-        ).text
+                """
+                Title and tag updated
+                """
+                card_update_text = self.browser.find_element_by_tag_name(
+                    'div.item-row > div > strike'
+                )
+                assert title_val + ' edit' in card_update_text.text
 
-        """
-        The cancel button sent us to the main page
-        """
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'cancel_button'))
-        ).click()
-        assert 'Создать' in browser.find_element_by_name(
-            'create_task'
-        ).get_attribute('value')
+                """
+                Description updated
+                """
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'edit'))
+                ).click()
+                assert desc_val + ' edit' in self.browser.find_element_by_id(
+                    'id_description'
+                ).text
 
-        """
-        Deleting task
-        """
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'delete'))
-        ).click()
+                """
+                The cancel button sent us to the main page
+                """
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((
+                        By.CLASS_NAME, 'cancel_button'
+                    ))
+                ).click()
+                assert 'Создать' in self.browser.find_element_by_name(
+                    'create_task'
+                ).get_attribute('value')
 
-        message_delete = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
-        )
-        assert 'Удалить задачу' in message_delete.text
+                """
+                Deleting task
+                """
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'delete'))
+                ).click()
 
-        """
-        Check if the visible description and buttons, deleting the task
-        """
-        delete_text = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.TAG_NAME, 'p'))
-        )
-        assert delete_text
+                message_delete = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
+                )
+                assert 'Удалить задачу' in message_delete.text
 
-        cancel_delete = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'cancel_button'))
-        )
-        assert cancel_delete
+                """
+                Check if the visible description and buttons, deleting the task
+                """
+                delete_text = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.TAG_NAME, 'p'))
+                )
+                assert delete_text
 
-        WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.NAME, 'confirm'))
-        ).click()
+                cancel_delete = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((
+                        By.CLASS_NAME, 'cancel_button'
+                    ))
+                )
+                assert cancel_delete
 
-        """
-        Message on the main page after delete
-        """
-        message_success_delete = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
-        )
-        assert 'Задача удалена!' in message_success_delete.text
+                WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.NAME, 'confirm'))
+                ).click()
 
-        """
-        Checking the number of cards
-        """
-        number_cards = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.TAG_NAME, 'p'))
-        )
-        assert "{} карточек".format(int(cards_val)) in number_cards.text
+                """
+                Message on the main page after delete
+                """
+                message_success_delete = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'alert'))
+                )
+                assert 'Задача удалена!' in message_success_delete.text
 
-    finally:
-        browser.quit()
+                """
+                Checking the number of cards
+                """
+                number_cards = WebDriverWait(self.browser, 3).until(
+                    EC.visibility_of_element_located((By.TAG_NAME, 'p'))
+                )
+                assert "{} карточек".format(int(cards_val)) in number_cards.text
 
-
-selenium_test(webdriver.Chrome())
-selenium_test(webdriver.Firefox())
+            finally:
+                self.browser.quit()
